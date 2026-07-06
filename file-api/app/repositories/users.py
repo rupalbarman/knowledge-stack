c0faeac1e1adff8a3ea706aa16779f7aec946ef8
Rupal <rupalbarman@gmail.com>
2026-07-06 16:51:59 +0700

    fix: fix typo in user-repo

--- file-api/app/repositories/users.py
+++ file-api/app/repositories/users.py
@@ -9,7 +9,7 @@ async def get_by_email(conn: DBConnection, email: str) -> asyncpg.Record | None:
     return await conn.fetchrow("SELECT * FROM users WHERE email = $1", email)
 
 
-async def get_by_id(conn: asyncpg.Connection, user_id: str) -> asyncpg.Record | None:
+async def get_by_id(conn: DBConnection, user_id: UUID) -> asyncpg.Record | None:
     return await conn.fetchrow("SELECT * FROM users WHERE id = $1", user_id)
 
 
