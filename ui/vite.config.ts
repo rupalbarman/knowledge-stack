0cec5a6ebaf5d85cb3fca385bcb4bcf4c345d60d
Rupal <rupalbarman@gmail.com>
2026-07-27 14:16:11 +0700

    fix: allow saq queue monitor ui to be accessible from frontend

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -3,17 +3,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+const BACKEND_API_URL = "http://127.0.0.1:8000";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8000",
+        target: BACKEND_API_URL,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
         ws: true,
       },
+      "/monitor": { target: BACKEND_API_URL },
     },
   },
   resolve: {
